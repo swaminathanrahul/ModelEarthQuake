f11512116ad8eebf42fa86b03bcc2f0265498c91
EILSEQ <tools@eilseq.com>
2024-04-11 20:52:02 +0200

    move from notebook to classic script

---
+++ main.py
@@ -0,0 +1,17 @@
+import sys
+
+from src.features import get_features
+from src.evaluation import evaluate_model
+from src.submission import generate_submission
+
+param_grid = {
+    'randomforestclassifier__n_estimators': [50, 100],
+    'randomforestclassifier__min_samples_leaf': [1, 5]
+}
+
+if __name__ == "__main__":
+    categories = sys.argv[1:]
+    selected_features = get_features(*categories)
+    predictions, score = evaluate_model(selected_features, param_grid)
+    print(score)
+    generate_submission(predictions)
