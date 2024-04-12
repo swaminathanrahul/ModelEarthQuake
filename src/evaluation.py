11954fa107d7f4a78a19a55fbfa335efbd6102ff
EILSEQ <tools@eilseq.com>
2024-04-12 12:54:46 +0200

    remove legacy

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,8 +1,5 @@
 import pandas as pd
 
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import f1_score
-
 from src.prepare_data import load_data
 from src.prediction import generate_prediction
 
