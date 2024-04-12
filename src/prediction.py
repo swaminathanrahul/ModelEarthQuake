9a42778c5d0363d9bbf271e4d320f06bbd7e1d6d
EILSEQ <tools@eilseq.com>
2024-04-12 11:47:13 +0200

    data profile for each job

--- src/prediction.py
+++ src/prediction.py
@@ -1,5 +1,6 @@
 from src.model import train_model
 from src.prepare_data import preprocess_data
+from src.profiling import create_profile
 
 
 def generate_prediction(X_train, X_test, y_train, selected_features, param_grid):
@@ -8,4 +9,10 @@ def generate_prediction(X_train, X_test, y_train, selected_features, param_grid)
 
     gs = train_model(X_train_subset, y_train, param_grid)
 
+<<<<<<< Updated upstream
     return gs.predict(X_test_subset)
+=======
+    create_profile(X_train_subset, y_train)
+
+    return gs, gs.predict(X_test_subset)
+>>>>>>> Stashed changes
