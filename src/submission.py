a4881b76987b098507a60f8d6ee2ce1786091de2
EILSEQ <tools@eilseq.com>
2024-04-12 07:46:48 +0200

    update remote pipeline to scrape best result

--- src/submission.py
+++ src/submission.py
@@ -2,16 +2,21 @@ import pandas as pd
 from src.prepare_data import DATA_DIR
 
 
-def generate_submission(predictions):
+def generate_submission(predictions, *categories):
     submission_format = pd.read_csv(
         DATA_DIR / 'submission_format.csv', index_col='building_id')
+
     required_length = len(submission_format)
 
-    # Adjust predictions to match required length
     if len(predictions) > required_length:
         predictions = predictions[:required_length]  # Truncate if too long
+
+    categories_str = '_'.join(categories).replace(
+        ' ', '_')  # Replace spaces with underscores if any
+
+    filename = f'dist/submission-{categories_str}.csv'
 
     result = pd.DataFrame(
         data=predictions, columns=submission_format.columns, index=submission_format.index)
 
-    result.to_csv('dist/submission.csv', index=True)
+    result.to_csv(filename, index=True)
