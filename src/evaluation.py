1673cc11c03026bd57a081b66a104dfca7150ed0
Rahul S <s_rahul@yahoo.com>
2024-04-12 11:02:09 +0200

    Using CV from GriSearch to run cross validation rather than traditional train test split.

--- src/evaluation.py
+++ src/evaluation.py
@@ -8,14 +8,14 @@ from src.prediction import generate_prediction
 
 
 def evaluate_model(selected_features, param_grid):
-    train_values, train_labels, _ = load_data()
+    train_values, train_labels, test_values = load_data()
 
-    sub_train_values, sub_test_values, sub_train_labels, sub_test_labels = train_test_split(
-        train_values, train_labels, test_size=0.5, random_state=42)
+    # sub_train_values, sub_test_values, sub_train_labels, sub_test_labels = train_test_split(
+    #     train_values, train_labels, test_size=0.5, random_state=42)
 
-    test_preds = generate_prediction(
-        sub_train_values, sub_test_values, sub_train_labels, selected_features, param_grid)
+    gs, test_preds = generate_prediction(
+        train_values, test_values, train_labels, selected_features, param_grid)
 
-    test_f1_score = f1_score(sub_test_labels, test_preds, average='micro')
+    test_f1_score = gs.best_score_
 
     return test_preds, test_f1_score
