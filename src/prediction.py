43f7df6cf9c8295fe91b50d3dd44e32b36245cfe
EILSEQ <tools@eilseq.com>
2024-04-12 13:29:27 +0200

    revert and abbandone profile generation
    - profiling library incompatible with environment

--- src/prediction.py
+++ src/prediction.py
@@ -1,6 +1,5 @@
 from src.model import train_model
 from src.prepare_data import preprocess_data
-from src.profiling import create_profile
 
 
 def generate_prediction(X_train, X_test, y_train, selected_features, param_grid):
@@ -8,7 +7,5 @@ def generate_prediction(X_train, X_test, y_train, selected_features, param_grid)
     X_test_subset = preprocess_data(X_test, selected_features)
 
     gs = train_model(X_train_subset, y_train, param_grid)
-
-    create_profile(X_train_subset, y_train)
 
     return gs, gs.predict(X_test_subset)
