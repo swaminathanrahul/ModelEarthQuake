43a0e8e138da450bcde6aa0ce21b1d38236d768c
Rahul S <s_rahul@yahoo.com>
2024-04-12 11:22:32 +0200

    removing commented code thats no longer used.

--- src/evaluation.py
+++ src/evaluation.py
@@ -10,9 +10,6 @@ from src.prediction import generate_prediction
 def evaluate_model(selected_features, param_grid):
     train_values, train_labels, test_values = load_data()
 
-    # sub_train_values, sub_test_values, sub_train_labels, sub_test_labels = train_test_split(
-    #     train_values, train_labels, test_size=0.5, random_state=42)
-
     gs, test_preds = generate_prediction(
         train_values, test_values, train_labels, selected_features, param_grid)
 
